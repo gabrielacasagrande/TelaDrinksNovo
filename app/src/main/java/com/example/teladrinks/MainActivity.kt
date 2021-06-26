e263f5f88f5f67922630d9be64e7e5adfb95cd13
Gabriela Casagrande <gabriela_casagrande86@hotmail.com>
2021-06-26 09:59:40 -0300

    Collapse Layout funcionando

--- app/src/main/java/com/example/teladrinks/MainActivity.kt
+++ app/src/main/java/com/example/teladrinks/MainActivity.kt
@@ -7,15 +7,16 @@ import android.os.Bundle
 import android.widget.Toolbar
 import androidx.palette.graphics.Palette
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-      //val bitmap: Bitmap = BitmapFactory.decodeResource(resources,R.drawable.drink)
-        //      Palette.from(bitmap).generate{palette ->
-        //    if(palette != null){
-        //        collapsingToobar.setContentScrimColor(palette.getMutedColor(R.attr.colorPrimary))
-        //   }
-        }
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbarLayout)
+        setSupportActionBar(toolbar)
+
+
     }
+}
