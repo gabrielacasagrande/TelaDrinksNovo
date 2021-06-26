a3d5a6f27ca0ef7d37dffef5dc6247ae209e2286
Gabriela Casagrande <gabriela_casagrande86@hotmail.com>
2021-06-26 00:17:10 -0300

    Collapse Layout pronto

--- app/src/main/java/com/example/teladrinks/MainActivity.kt
+++ app/src/main/java/com/example/teladrinks/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.teladrinks
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.palette.graphics.Palette
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+      //val bitmap: Bitmap = BitmapFactory.decodeResource(resources,R.drawable.drink)
+        //      Palette.from(bitmap).generate{palette ->
+        //    if(palette != null){
+        //        collapsingToobar.setContentScrimColor(palette.getMutedColor(R.attr.colorPrimary))
+        //   }
+        }
     }
-}
